dc1869c2c628e7dc133a4a998d4364f5a039a210
TimePath <andrew.hardaker1995@gmail.com>
2015-01-07 13:01:09 +1100

    Merge terminal definitions

--- src/main/kotlin/com/timepath/quakec/ast/impl/Terminal.kt
+++ src/main/kotlin/com/timepath/quakec/ast/impl/Terminal.kt
@@ -1,20 +1,19 @@
 package com.timepath.quakec.ast.impl
 
 import com.timepath.quakec.ast.Expression
+import com.timepath.quakec.vm.Instruction
+import com.timepath.quakec.ast.Value
 import com.timepath.quakec.ast.GenerationContext
 import com.timepath.quakec.ast.IR
-import com.timepath.quakec.ast.Value
 
-import com.timepath.quakec.vm.Instruction
-
-class ConstantExpression(va: Any) : Expression() {
+class ConstantExpression(any: Any) : Expression() {
 
     val instr: Map<Class<*>, Instruction> = mapOf(
             javaClass<String>() to Instruction.STORE_STR,
             javaClass<Float>() to Instruction.STORE_FLOAT
     )
 
-    val value = Value(va)
+    val value = Value(any)
 
     override val attributes: Map<String, Any?>
         get() = mapOf("value" to value)
@@ -26,4 +25,19 @@ class ConstantExpression(va: Any) : Expression() {
     }
 
     override fun toString(): String = value.toString()
+}
+
+open class ReferenceExpression(val id: String) : Expression() {
+
+    override val attributes: Map<String, Any>
+        get() = mapOf("id" to id)
+
+    override fun generate(ctx: GenerationContext): List<IR> {
+        if (id in ctx.registry)
+            return listOf(IR(ret = ctx.registry[id], dummy = true))
+        return super.generate(ctx)
+    }
+
+    override fun toString(): String = id
+
 }
