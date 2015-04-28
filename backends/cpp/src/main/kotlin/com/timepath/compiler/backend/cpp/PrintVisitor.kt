5fef258468ca8e2450a0f0d98926226743ff6a81
TimePath <andrew.hardaker1995@gmail.com>
2015-04-28 13:20:36 +1000

    Fix indentation

--- backends/cpp/src/main/kotlin/com/timepath/compiler/backend/cpp/PrintVisitor.kt
+++ backends/cpp/src/main/kotlin/com/timepath/compiler/backend/cpp/PrintVisitor.kt
@@ -8,19 +8,34 @@ import com.timepath.compiler.backend.q1vm.types.*
 import com.timepath.compiler.types.Type
 import com.timepath.compiler.types.defaults.function_t
 
-class PrintVisitor(val state: Q1VM.State, val indent: String = "    ") : ASTVisitor<String> {
+class PrintVisitor(val state: Q1VM.State, val indent: String = "    ") : ASTVisitor<Printer> {
 
     fun block(l: List<Expression>) = Printer {
         +"{"
-        +indent { l.forEach { +(it.print() + ";") } }
+        +indent {
+            l.forEach {
+                val line = it.print()
+                when {
+                    it is ConditionalExpression && !it.expression -> Unit
+                    it is FunctionExpression && it.children.isNotEmpty() -> Unit
+                    it is LabelExpression -> Unit
+                    it is SwitchExpression.Case -> Unit
+                    else -> line.terminate(";")
+                }
+                +line
+            }
+        }
         +"}"
     }
 
     override fun default(e: Expression) = throw NullPointerException("${e.javaClass}")
 
-    override fun visit(e: Nop) = ";"
-    override fun visit(e: UnaryExpression) = "${e.op}${e.operand.print()}"
-    override fun visit(e: UnaryExpression.Cast) = "(${typename(e.type)}) ${e.operand.print()}"
+    val String.p: Printer
+        get() = Printer(this)
+
+    override fun visit(e: Nop) = ";".p
+    override fun visit(e: UnaryExpression) = "${e.op}${e.operand.print()}".p
+    override fun visit(e: UnaryExpression.Cast) = "(${typename(e.type)}) ${e.operand.print()}".p
     val binaryPrecedence = mapOf(
             "::" to 1
             , "()" to 2
@@ -46,17 +61,17 @@ class PrintVisitor(val state: Q1VM.State, val indent: String = "    ") : ASTVisi
     )
 
     fun Expression.printPrec(parent: String) = when {
-        this is BinaryExpression && binaryPrecedence[op]!! > binaryPrecedence[parent]!! -> "(${print()})"
+        this is BinaryExpression && binaryPrecedence[op]!! > binaryPrecedence[parent]!! -> "(${print()})".p
         else -> print()
     }
 
-    override fun visit(e: BinaryExpression) = "${e.left.printPrec(e.op)} ${e.op} ${e.right.printPrec(e.op)}"
+    override fun visit(e: BinaryExpression) = "${e.left.printPrec(e.op)} ${e.op} ${e.right.printPrec(e.op)}".p
 
-    override fun visit(e: MemberExpression) = "${e.left.print()}.${e.field.id}"
-    override fun visit(e: MemberReferenceExpression) = "${typename(e.owner)}::${e.id}"
+    override fun visit(e: MemberExpression) = "${e.left.print()}.${e.field.id}".p
+    override fun visit(e: MemberReferenceExpression) = "${typename(e.owner)}::${e.id}".p
     override fun visit(e: IndexExpression) = when {
-        e.right.type() is field_t -> "${e.left.print()}.*${e.right.print()}"
-        else -> "${e.left.print()}[${e.right.print()}]"
+        e.right.type() is field_t -> "${e.left.print()}.*${e.right.print()}".p
+        else -> "${e.left.print()}[${e.right.print()}]".p
     }
 
     suppress("NOTHING_TO_INLINE") inline fun Expression.type() = accept(state.typeVisitor)
@@ -77,13 +92,13 @@ class PrintVisitor(val state: Q1VM.State, val indent: String = "    ") : ASTVisi
     fun _declare_field(t: field_t, id: String?)
             = _declare(t.type, "entity::*${id ?: ""}", null)
 
-    fun _declare(t: Type, id: String?, v: Expression?): String = when (t) {
-        is array_t -> _declare(t.type, id, null) +
+    fun _declare(t: Type, id: String?, v: Expression?): Printer = when (t) {
+        is array_t -> _declare(t.type, id, null).toString() +
                 "[${v!!.print()}]"
-        is field_t -> _declare_field(t, id) +
+        is field_t -> _declare_field(t, id).toString() +
                 (if (v != null) " = (${_declare_field(t, null)}) ${v.print()}" else "")
         is function_t -> "${_declare(t.type, "(*${id ?: ""})", null)}" +
-                "(${t.argTypes.map { _declare(it, null, null) }.join(", ")}" +
+                "(${t.argTypes.map { _declare(it, null, null).toString() }.join(", ")}" +
                 (if (t.argTypes.isNotEmpty() && t.vararg != null) ", " else "") +
                 (t.vararg ?: "") +
                 ")"
@@ -91,9 +106,9 @@ class PrintVisitor(val state: Q1VM.State, val indent: String = "    ") : ASTVisi
                 (if (id != null) " ${id}" else "") +
                 (if (v != null) " = ${v.print()}"
                 else "")
-    }
+    }.p
 
-    fun declare(e: DeclarationExpression): String {
+    fun declare(e: DeclarationExpression): Printer {
         val t = e.type
         return when (t) {
             is array_t -> _declare(t, e.id, t.sizeExpr)
@@ -102,97 +117,99 @@ class PrintVisitor(val state: Q1VM.State, val indent: String = "    ") : ASTVisi
         }
     }
 
-    override fun visit(e: BlockExpression) = block(e.children).toString()
+    override fun visit(e: BlockExpression) = block(e.children).toString().p
 
-    override fun visit(e: BreakStatement) = "break"
+    override fun visit(e: BreakStatement) = "break".p
 
-    fun compound(e: Expression, s: Boolean) = when (e) {
+    fun compound(e: Expression) = when (e) {
         is BlockExpression -> e.print()
-        is ConditionalExpression -> (if (s) "\n" else "") + e.print()
-        else -> Printer {
-            +"{"
-            +indent { +(e.print() + ";") }
-            +"}"
-        }.toString()
+        is ConditionalExpression -> e.print()
+        else -> block(listOf(e))
     }
 
-    override fun visit(e: ConditionalExpression): String {
+    override fun visit(e: ConditionalExpression): Printer {
         val pred = e.test.print()
         return when {
             e.expression -> {
                 val pass = e.pass.print()
-                check(e.fail != null, "f is null")
                 val fail = e.fail!!.print()
-                "$pred ? $pass : $fail"
+                "$pred ? $pass : $fail".p
             }
-            else -> "if ($pred) ${compound(e.pass, true)}" + run {
-                if (e.fail != null) " else ${compound(e.fail, false)}" else ""
+            else -> Printer {
+                run {
+                    +"if ($pred)"
+                    +compound(e.pass)
+                }
+                if (e.fail != null) {
+                    +"else"
+                    +compound(e.fail)
+                }
             }
         }
     }
 
     override fun visit(e: ConstantExpression) = e.value.any.let {
         when (it) {
-            is Pointer -> "${it.int}"
-            is Float -> "${it}f"
-            is Int -> "${it}"
-            is Vector -> "(vector) { ${it.x}f, ${it.y}f, ${it.z}f }"
-            is Char -> "'${it}'"
-            is String -> "\"${it}\""
+            is Pointer -> "${it.int}".p
+            is Float -> "${it}f".p
+            is Int -> "${it}".p
+            is Vector -> "(vector) { ${it.x}f, ${it.y}f, ${it.z}f }".p
+            is Char -> "'${it}'".p
+            is String -> "\"${it}\"".p
             else -> throw NoWhenBranchMatchedException()
         }
     }
 
-    override fun visit(e: ContinueStatement) = "continue"
+    override fun visit(e: ContinueStatement) = "continue".p
 
     override fun visit(e: DeclarationExpression) = declare(e)
     override fun visit(e: StructDeclarationExpression) = declare(e)
     override fun visit(e: ParameterExpression) = declare(e)
 
     override fun visit(e: FunctionExpression) = Printer {
-        val pars = e.params?.map { it.print() }
-                ?: e.type.argTypes.map { _declare(it, null, null) }
-        val vara = e.vararg?.let { it.print().let { listOf(it) } }
-                ?: e.type.vararg?.let { _declare(it, null, null).let { listOf(it) } }
+        val pars = e.params?.map { it.print().toString() }
+                ?: e.type.argTypes.map { _declare(it, null, null).toString() }
+        val vara = e.vararg?.let { it.print().toString().let { listOf(it) } }
+                ?: e.type.vararg?.let { _declare(it, null, null).toString().let { listOf(it) } }
                 ?: emptyList()
         +_declare(e.type.type, "${e.id}(${(pars + vara).join(", ")})", null)
         if (e.children.isNotEmpty()) {
             +block(e.children)
         }
-    }.toString()
+    }
 
-    override fun visit(e: GotoExpression) = "goto ${e.id}"
+    override fun visit(e: GotoExpression) = "goto ${e.id}".p
 
-    override fun visit(e: LabelExpression) = "${e.id}:"
+    override fun visit(e: LabelExpression) = "${e.id}:".p
 
-    override fun visit(e: LoopExpression): String {
-        val init = e.initializer?.let { it.map { it.print() }.join(", ") }
+    override fun visit(e: LoopExpression): Printer {
+        val init = e.initializer?.let { it.map { it.print().toString() }.join(", ") }
         val pred = e.predicate.print()
         val body = block(e.children)
-        val update = e.update?.let { it.map { it.print() }.join(", ") }
+        val update = e.update?.let { it.map { it.print().toString() }.join(", ") }
         return when {
             !e.checkBefore -> "do $body while ($pred)"
             e.initializer != null && e.update != null -> "for ($init; $pred; $update) $body"
             else -> "while ($pred) $body"
-        }
+        }.p
     }
 
-    override fun visit(e: MethodCallExpression) = "${e.function.print()}(${e.args.map { it.print() }.join(", ")})"
+    override fun visit(e: MethodCallExpression) = "${e.function.print()}(${e.args.map { it.print().toString() }.join(", ")})".p
 
-    override fun visit(e: ReferenceExpression) = "${e.refers.id}"
+    override fun visit(e: ReferenceExpression) = "${e.refers.id}".p
 
     override fun visit(e: ReturnStatement) = when {
         e.returnValue == null -> "return"
         else -> "return ${e.returnValue.print()}"
-    }
+    }.p
 
     override fun visit(e: SwitchExpression) = Printer {
         +"switch (${e.test.print()})"
         +block(e.children)
-    }.toString()
+    }
 
     override fun visit(e: SwitchExpression.Case) = when (e.expr) {
         null -> "default:"
         else -> "case ${e.expr.print()}:"
-    }
+    }.p
 }
--- backends/cpp/src/main/kotlin/com/timepath/compiler/backend/cpp/Printer.kt
+++ backends/cpp/src/main/kotlin/com/timepath/compiler/backend/cpp/Printer.kt
@@ -3,13 +3,20 @@ package com.timepath.compiler.backend.cpp
 public class Printer private(private val indent: String) {
     private val lines: MutableList<String> = linkedListOf()
     override fun toString() = lines.sequence().map { indent + it }.join("\n")
-    fun T.plus<T>() = this@Printer.lines.add(this@plus.toString()).let { Unit }
+    fun T.plus<T>() = this@Printer.lines.addAll(this@plus.toString().split('\n')).let { Unit }
     inline fun String.invoke(body: Printer.() -> Unit) = Printer(this, body)
+
+    fun terminate(s: String): Printer {
+        lines[lines.lastIndex] += s
+        return this
+    }
 
     companion object {
         inline fun invoke(indent: String = "", body: Printer.() -> Unit) = Printer(indent).let {
             it.body()
             it
         }
+
+        fun invoke(body: String = "") = Printer("") { +body }
     }
 }
